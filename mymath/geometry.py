833d4f1888e39c10f0016aeea88c86028ef8d558
Abitatha Roy <abitatharoy@gmail.com>
2025-05-17 04:24:12 +0530

    First commit

---
+++ mymath/geometry.py
@@ -0,0 +1,15 @@
+# basic geometrical operations
+
+import math
+
+def area_of_rectangle(a, b):
+    """
+    This function returns the area of the input rectangle.
+    """
+    return a * b
+
+def area_of_circle(r):
+    """
+    This function returns the area of the input circle.
+    """
+    return math.pi * (r ** 2)
