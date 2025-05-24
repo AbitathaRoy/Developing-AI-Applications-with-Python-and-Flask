625e14b752d1b49114a85958abe71a970420b79e
Abitatha Roy <abitatharoy@gmail.com>
2025-05-24 20:41:24 +0530

    Added basic math app config

---
+++ project/practice_project/Maths/mathematics.py
@@ -0,0 +1,14 @@
+
+
+def addition(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
